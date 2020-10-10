34c25d2686abe2a8c1d542eca6f7524d34702072
Sam Hall <mann.samantha11@gmail.com>
2020-10-10 22:36:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,3 @@ color = [red, orange, yellow, green, blue, indigo, violet]
 def display_rainbow
   puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, /v: #{color[6]}"
 end
-
-  
